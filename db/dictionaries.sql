c002c5cfbe329e9ff772249f9206cc7ddf233a54
Przemek <przemastro@gmail.com>
2016-10-20 20:10:35 +0200

    added dictionaries.sql query

---
+++ db/dictionaries.sql
@@ -0,0 +1,13 @@
+  insert into [Astro].[dic].[ConversionTypes] values(1,'Linear')
+  insert into [Astro].[dic].[ConversionTypes] values(2,'Power')
+  insert into [Astro].[dic].[ConversionTypes] values(3,'Hist')
+
+  insert into [Astro].[dic].[FileExtensions] values(1, 'jpg')
+  insert into [Astro].[dic].[FileExtensions] values(2, 'png')
+  insert into [Astro].[dic].[FileExtensions] values(3, 'fits')
+
+  insert into [Astro].[dic].[ImageTypes] values(1, 'Raw')
+  insert into [Astro].[dic].[ImageTypes] values(2, 'Dark')
+  insert into [Astro].[dic].[ImageTypes] values(3, 'Flat')
+  insert into [Astro].[dic].[ImageTypes] values(4, 'Bias')
+  insert into [Astro].[dic].[ImageTypes] values(5, 'Processed')
